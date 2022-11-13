b33ef7b4e6c2b8318221db009b950f00950546c8
Gor <100875713+Goratsy@users.noreply.github.com>
2022-11-13 17:35:30 +0300

    add post + footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEventComponent } from './components/create-event/create-event.component';
+import { HomeComponent } from './components/home/home.component';
+import { LovesComponent } from './components/loves/loves.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
-  
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'loves', component: LovesComponent },
+  { path: 'create-event', component: CreateEventComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LovesComponent } from './components/loves/loves.component';
+import { CreateEventComponent } from './components/create-event/create-event.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostComponent } from './components/home/post/post.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LovesComponent,
+    CreateEventComponent,
+    NotFoundComponent,
+    HomeComponent,
+    PostComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
